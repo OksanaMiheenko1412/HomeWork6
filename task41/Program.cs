23ef5425c220ab84de799e39dba151919c2817e5
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-11 13:34:14 +0700

    / add  commit task41

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// task41 Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел
+// больше 0 ввел пользователь. 0,7,8,-2,-2 ->2; -1,-7,567,89,223 ->3
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 1000);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
+
+
